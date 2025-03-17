c3386ac4de1090149dbc16eb8a0cc29d33a7c618
Vallarasu4653 <vallarasu.ravichandran@syncfusion.com>
2025-03-17 12:55:39 +0530

    added the blog sample

---
+++ StockChart/StockChart/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace StockChart
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SfSegmentedControl_SelectionChanged(object sender, Syncfusion.Maui.Toolkit.SegmentedControl.SelectionChangedEventArgs e)
+        {
+            if (e.NewValue is Syncfusion.Maui.Toolkit.SegmentedControl.SfSegmentItem selectedItem)
+            {
+                DateTime startDate = new DateTime(2024, 01, 01);
+                DateTime endDate = startDate;
+
+                switch (selectedItem.Text)
+                {
+                    case "3m":
+                        endDate = startDate.AddMonths(3);
+                        break;
+                    case "6m":
+                        endDate = startDate.AddMonths(6);
+                        break;
+                    case "9m":
+                        endDate = startDate.AddMonths(9);
+                        break;
+                    case "1y":
+                        endDate = startDate.AddYears(1);
+                        break;
+                    case "All":
+                        startDate = new DateTime(2023, 11, 08);
+                        endDate = new DateTime(2025, 02, 07);
+                        break;
+                }
+
+                viewModel.RangeStart = startDate;
+                viewModel.RangeEnd = endDate;
+            }
+            segmentedControl.SelectionIndicatorSettings = new Syncfusion.Maui.Toolkit.SegmentedControl.SelectionIndicatorSettings
+            {
+                Background = Colors.White,
+                TextColor = Colors.Black
+            };
+        }
+    }
+}
---
+++ StockChart/StockChart/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
+using Syncfusion.Maui.Toolkit.Hosting;
+
+namespace StockChart
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureSyncfusionToolkit()
+                .ConfigureSyncfusionCore()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
