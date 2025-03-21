665092b6f13007dba3e135ffda81daff908e525c
Vallarasu4653 <vallarasu.ravichandran@syncfusion.com>
2025-03-21 10:02:39 +0530

    update on folder structure

---
+++ StockChart/StockChart/Model/StockModel.cs
@@ -0,0 +1,21 @@
+namespace StockChart;
+
+public class StockModel
+{
+    public DateTime Date { get; }
+    public double Open { get; }
+    public double High { get; }
+    public double Low { get; }
+    public double Close { get; }
+    public double Volume { get; }
+
+    public StockModel(DateTime date, double open, double high, double low, double close, double volume)
+    {
+        Date = date;
+        Open = open;
+        High = high;
+        Low = low;
+        Close = close;
+        Volume = volume;
+    }
+}
